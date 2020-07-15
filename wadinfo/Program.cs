ce42d2b5e8bb5919b38019ec1b2d1696c7a4f0b9
Richard Moss <richard.moss@cyotek.com>
2020-07-15 08:02:12 +0100

    Added WadInfo tool

---
+++ wadinfo/Program.cs
@@ -0,0 +1,45 @@
+﻿using Cyotek.Data.Wad;
+using System;
+using System.IO;
+
+namespace Cyotek.Tools.WadInfo
+{
+  internal static class Program
+  {
+    #region Public Methods
+
+    public static void Main(string[] args)
+    {
+      for (int i = 0; i < args.Length; i++)
+      {
+        Program.WriteWadInfo(Path.Combine(Environment.CurrentDirectory, args[i]));
+      }
+    }
+
+    #endregion Public Methods
+
+    #region Private Methods
+
+    private static void WriteWadInfo(string fileName)
+    {
+      using (Stream stream = File.OpenRead(fileName))
+      {
+        WadReader reader;
+        WadLump lump;
+
+        reader = new WadReader(stream);
+
+        Console.WriteLine("File: {0}", fileName);
+        Console.WriteLine("Type: {0}", reader.Type);
+        Console.WriteLine("Lump Count: {0}", reader.Count);
+
+        while ((lump = reader.GetNextLump()) != null)
+        {
+          Console.WriteLine("{0}: Offset {1}, Size {2}", lump.Name, lump.Offset, lump.Size);
+        }
+      }
+    }
+
+    #endregion Private Methods
+  }
+}
---
+++ wadinfo/Properties/AssemblyInfo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("WAD Info")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Cyotek Ltd")]
+[assembly: AssemblyProduct("Cyotek WadTools")]
+[assembly: AssemblyCopyright("Copyright © 2020 Cyotek Ltd. All Rights Reserved.")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: CLSCompliant(true)]
+[assembly: Guid("42e4dc87-b86b-42c0-9917-1373a7d73c4f")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
